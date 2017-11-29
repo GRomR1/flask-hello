74660561690b49ff4c8e3be54112a9b617843fd9
Ruslan Gainanov <gromrx1@gmail.com>
2017-11-29 13:22:08 +0500

    Change to listen all interfaces

--- hello.py
+++ hello.py
@@ -7,10 +7,10 @@ def hello():
     return "Hello, World!"
   
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 	
 # Use it to run: 
 # python hello.py
 # 
 # After that will output:
-#  * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+#  * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
